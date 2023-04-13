cd0cbaaa97e8c4fcf5c7a29d68c286387ceda949
lunainfotec <kirtimadan1997@gmail.com>
2023-04-13 12:22:48 +0530

    added : Employe Attendance

--- src/com/bl/employeewage/EmployeeWage.java
+++ src/com/bl/employeewage/EmployeeWage.java
@@ -1,8 +1,23 @@
 package com.bl.employeewage;
 
+
+class EmployeeAttendance{
+
+    int random = (int)(Math.random() * 10 %2);
+    int is_preset = 1;
+}
+
 public class EmployeeWage {
 
     public static void main(String[] args) {
         System.out.println("Displaying Welcome to Employee Wage Computation Program");
+
+        EmployeeAttendance obj = new EmployeeAttendance();
+        System.out.println("random :" + obj.random);
+        if (obj.is_preset == 1){
+            System.out.println("Employee is present");
+        }else {
+            System.out.println("Employee is not present");
+        }
     }
 }
